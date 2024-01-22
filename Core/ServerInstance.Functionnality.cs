070a0214a5a713c1739b8c620ec4992a6c06366c
Émile Desjardins <desjardins.e@outlook.com>
2024-01-21 21:19:19 -0500

    make server instance partial class with functionnality separated

---
+++ Core/ServerInstance.Functionnality.cs
@@ -0,0 +1,80 @@
+﻿using Core.Application;
+using PlatypusAPI.ApplicationAction.Run;
+using PlatypusAPI.ApplicationAction;
+using PlatypusAPI.User;
+using PlatypusApplicationFramework.Core.ApplicationAction;
+using PlatypusApplicationFramework.Core.Event;
+
+namespace Core
+{
+    public partial class ServerInstance
+    {
+        public UserAccount UserConnect(Dictionary<string, object> credential, string connectionMethodGuid)
+        {
+            return _usersHandler.Connect(credential, connectionMethodGuid);
+        }
+
+        public void InstallApplication(UserAccount userAccount, string applicationPath)
+        {
+            ValidateUserForPermission(userAccount, UserPermissionFlag.InstallAndUninstallApplication);
+
+            _applicationsHandler.InstallApplication(applicationPath);
+        }
+
+        public void UninstalApplication(UserAccount userAccount, string applicationGuid)
+        {
+            ValidateUserForPermission(userAccount, UserPermissionFlag.InstallAndUninstallApplication);
+
+            UninstallApplicationDetails details = _applicationsHandler.UninstallApplication(applicationGuid);
+            foreach (string actionGuid in details.ActionGuids)
+                _applicationActionsHandler.RemoveAction(actionGuid);
+            foreach (string userConnectionMethodGuid in details.UserConnectionMethodGuids)
+                _usersHandler.RemoveConnectionMethod(userConnectionMethodGuid);
+
+            _eventsHandler.RunEventHandlers<object>(EventHandlerType.AfterUninstallApplication, details.EventEnv, (exception) => throw exception);
+        }
+
+        public ApplicationActionRunResult RunAction(UserAccount userAccount, ApplicationActionRunParameter runActionParameter)
+        {
+            ValidateUserForPermission(userAccount, UserPermissionFlag.RunAction);
+
+            if (_applicationActionsHandler.HasActionWithGuid(runActionParameter.Guid) == false)
+            {
+                string message = Common.Utils.GetString("ApplicationActionNotFound", runActionParameter.Guid);
+                return new ApplicationActionRunResult()
+                {
+                    Message = message,
+                    Status = ApplicationActionRunResultStatus.Failed,
+                };
+            }
+            ApplicationActionEnvironmentBase env = _applicationActionsHandler.CreateStartActionEnvironment(runActionParameter.Guid);
+            env.ApplicationRepository = _applicationRepository;
+
+            return _applicationActionsHandler.RunAction(runActionParameter, env);
+        }
+
+        public UserAccount AddUser(UserAccount userAccount, UserCreationParameter userCreationParameter)
+        {
+            ValidateUserForPermission(userAccount, UserPermissionFlag.AddUser);
+            return _usersHandler.AddUser(userCreationParameter);
+        }
+
+        public void CancelRunningApplicationAction(UserAccount userAccount, string guid)
+        {
+            ValidateUserForPermission(userAccount, UserPermissionFlag.CancelRunningAction);
+            _applicationActionsHandler.CancelRunningAction(guid);
+        }
+
+        public IEnumerable<ApplicationActionRunInfo> GetRunningApplicationActions(UserAccount userAccount)
+        {
+            ValidateUserForPermission(userAccount, UserPermissionFlag.GetRunningActions);
+            return _applicationActionsHandler.GetRunningApplicationActionInfos();
+        }
+
+        public IEnumerable<ApplicationActionInfo> GetApplicationActionInfos(UserAccount userAccount)
+        {
+            ValidateUserForPermission(userAccount, UserPermissionFlag.GetActionsInfo);
+            return _applicationActionsHandler.GetApplicationActionInfos();
+        }
+    }
+}
--- Core/ServerInstance.cs
+++ Core/ServerInstance.cs
@@ -9,16 +9,12 @@ using Logging;
 using Newtonsoft.Json;
 using Persistance;
 using Persistance.Repository;
-using PlatypusAPI.ApplicationAction;
-using PlatypusAPI.ApplicationAction.Run;
 using PlatypusAPI.Exceptions;
 using PlatypusAPI.User;
-using PlatypusApplicationFramework.Core.ApplicationAction;
-using PlatypusApplicationFramework.Core.Event;
 
 namespace Core
 {
-    public class ServerInstance
+    public partial class ServerInstance
     {
         private ServerConfig _config;
 
@@ -99,74 +95,6 @@ namespace Core
         public void InitializeRestAPIHandler(string[] args)
         {
             _restAPIHandler.Initialize(args, _config.HttpPort, _config.RestAPIUserTokenTimeout);
-        }
-
-        public UserAccount UserConnect(Dictionary<string, object> credential, string connectionMethodGuid)
-        {
-            return _usersHandler.Connect(credential, connectionMethodGuid);
-        }
-
-        public void InstallApplication(UserAccount userAccount, string applicationPath)
-        {
-            ValidateUserForPermission(userAccount, UserPermissionFlag.InstallAndUninstallApplication);
-
-            _applicationsHandler.InstallApplication(applicationPath);
-        }
-
-        public void UninstalApplication(UserAccount userAccount, string applicationGuid)
-        {
-            ValidateUserForPermission(userAccount, UserPermissionFlag.InstallAndUninstallApplication);
-
-            UninstallApplicationDetails details = _applicationsHandler.UninstallApplication(applicationGuid);
-            foreach(string actionGuid in details.ActionGuids)
-                _applicationActionsHandler.RemoveAction(actionGuid);
-            foreach (string userConnectionMethodGuid in details.UserConnectionMethodGuids)
-                _usersHandler.RemoveConnectionMethod(userConnectionMethodGuid);
-
-            _eventsHandler.RunEventHandlers<object>(EventHandlerType.AfterUninstallApplication, details.EventEnv, (exception) => throw exception);
-        }
-
-        public ApplicationActionRunResult RunAction(UserAccount userAccount, ApplicationActionRunParameter runActionParameter)
-        {
-            ValidateUserForPermission(userAccount, UserPermissionFlag.RunAction);
-
-            if (_applicationActionsHandler.HasActionWithGuid(runActionParameter.Guid) == false)
-            {
-                string message = Common.Utils.GetString("ApplicationActionNotFound", runActionParameter.Guid);
-                return new ApplicationActionRunResult()
-                {
-                    Message = message,
-                    Status = ApplicationActionRunResultStatus.Failed,
-                };
-            }
-            ApplicationActionEnvironmentBase env = _applicationActionsHandler.CreateStartActionEnvironment(runActionParameter.Guid);
-            env.ApplicationRepository = _applicationRepository;
-
-            return _applicationActionsHandler.RunAction(runActionParameter, env);
-        }
-
-        public UserAccount AddUser(UserAccount userAccount, UserCreationParameter userCreationParameter)
-        {
-            ValidateUserForPermission(userAccount, UserPermissionFlag.AddUser);
-            return _usersHandler.AddUser(userCreationParameter);
-        }
-
-        public void CancelRunningApplicationAction(UserAccount userAccount, string guid)
-        {
-            ValidateUserForPermission(userAccount, UserPermissionFlag.CancelRunningAction);
-            _applicationActionsHandler.CancelRunningAction(guid);
-        }
-
-        public IEnumerable<ApplicationActionRunInfo> GetRunningApplicationActions(UserAccount userAccount)
-        {
-            ValidateUserForPermission(userAccount, UserPermissionFlag.GetRunningActions);
-            return _applicationActionsHandler.GetRunningApplicationActionInfos();
-        }
-
-        public IEnumerable<ApplicationActionInfo> GetApplicationActionInfos(UserAccount userAccount)
-        {
-            ValidateUserForPermission(userAccount, UserPermissionFlag.GetActionsInfo);
-            return _applicationActionsHandler.GetApplicationActionInfos();
         }
 
         private void ValidateUserForPermission(UserAccount userAccount, UserPermissionFlag userPermissionFlag)
