cd68baec7134e355b281e27f1f34bc631c31b44d
Émile Desjardins <desjardins.e@outlook.com>
2024-02-03 15:52:10 -0500

    Update ServerInstance.cs

--- Core/ServerInstance.cs
+++ Core/ServerInstance.cs
@@ -4,7 +4,6 @@ using Core.Network.RestAPI;
 using Core.Network;
 using Core.Abstract;
 using Core.Application.Abstract;
-using PlatypusFramework.Configuration.Application;
 using Core.User.Abstract;
 using PlatypusRepository;
 using Core.ApplicationAction.Abstract;
@@ -24,10 +23,10 @@ namespace Core
     public partial class ServerInstance : BackgroundService
     {
         private readonly ServerConfig _config;
-        private readonly IServerStarter<ApplicationsHandler> _applicationsServerStarter;
-        private readonly IServerStarter<RunningApplicationActionEntity> _runningApplicationActionsServerStarter;
         private readonly ISeverPortListener<RestAPIHandler> _restAPIHandler;
         private readonly ISeverPortListener<PlatypusServerSocketHandler> _tcpServerSocketHandler;
+        private readonly IServerStarter<ApplicationsHandler> _applicationsServerStarter;
+        private readonly IServerStarter<RunningApplicationActionEntity> _runningApplicationActionsServerStarter;
         private readonly IUserAuthentificator _userAuthentificator;
         private readonly IRepositoryAddOperator<UserCreationParameter> _userAddOperator;
         private readonly IRepositoryUpdateOperator<UserUpdateParameter> _userUpdateOperator;
@@ -35,7 +34,6 @@ namespace Core
         private readonly IRepositoryRemoveOperator<ApplicationActionRun, string> _applicationActionRunRemoveOperator;
         private readonly IRepositoryConsumeOperator<ApplicationActionRunInfo> _actionRunInfoConsumer;
         private readonly IRepositoryConsumeOperator<ApplicationActionInfo> _actionInfoConsumer;
-        private readonly IRepositoryAddOperator<PlatypusApplicationBase> _applicationAddOperator;
         private readonly IApplicationInstaller _applicationPackageInstaller;
         private readonly IApplicationUninstaller _applicationPackageUninstaller;
         private readonly IApplicationActionRunner _applicationActionRunner;
@@ -50,7 +48,6 @@ namespace Core
             IRepositoryRemoveOperator<ApplicationActionRun, string> applicationActionRunRemoveOperator,
             IRepositoryConsumeOperator<ApplicationActionRunInfo> actionRunInfoConsumer,
             IRepositoryConsumeOperator<ApplicationActionInfo> actionInfoConsumer,
-            IRepositoryAddOperator<PlatypusApplicationBase> applicationAddOperator,
             IApplicationInstaller applicationPackageInstaller,
             IApplicationUninstaller applicationPackageUninstaller,
             IApplicationActionRunner applicationActionRunner
@@ -58,6 +55,8 @@ namespace Core
         {
             string json = File.ReadAllText(ApplicationPaths.CONFIGFILEPATH);
             _config = JsonConvert.DeserializeObject<ServerConfig>(json);
+            _restAPIHandler = new RestAPIHandler(this, _config.RestAPIUserTokenTimeout);
+            _tcpServerSocketHandler = new PlatypusServerSocketHandler(this, ProtocolType.Tcp);
 
             _applicationsServerStarter = applicationsServerStarter;
             _runningApplicationActionsServerStarter = runningApplicationActionsServerStarter;
@@ -68,12 +67,9 @@ namespace Core
             _applicationActionRunRemoveOperator = applicationActionRunRemoveOperator;
             _actionRunInfoConsumer = actionRunInfoConsumer;
             _actionInfoConsumer = actionInfoConsumer;
-            _applicationAddOperator = applicationAddOperator;
             _applicationPackageInstaller = applicationPackageInstaller;
             _applicationPackageUninstaller = applicationPackageUninstaller;
             _applicationActionRunner = applicationActionRunner;
-            _restAPIHandler = new RestAPIHandler(this, _config.RestAPIUserTokenTimeout);
-            _tcpServerSocketHandler = new PlatypusServerSocketHandler(this, ProtocolType.Tcp);
         }
 
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
