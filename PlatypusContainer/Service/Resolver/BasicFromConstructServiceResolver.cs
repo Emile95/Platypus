f0f5f9415a2f262ed12568663cfa48f7c6399ef3
Émile Desjardins <desjardins.e@outlook.com>
2024-02-09 20:03:24 -0500

    add basic service (no singleton) for container service injection

---
+++ PlatypusContainer/Service/Resolver/BasicFromConstructServiceResolver.cs
@@ -0,0 +1,22 @@
+﻿namespace PlatypusContainer.Service.Resolver
+{
+    internal class BasicFromConstructServiceResolver : FromConstructServiceResolver, IServiceResolver
+    {
+        private readonly Type _serviceType;
+
+        public BasicFromConstructServiceResolver(
+            Type serviceType,
+            Type implmentationType
+        ) : base(implmentationType)
+        {
+            _serviceType = serviceType;
+        }
+
+        public bool Resolve(Dictionary<Type, Func<object>> serviceGetters)
+        {
+            if (serviceGetters.ContainsKey(_serviceType)) return false;
+            serviceGetters.Add(_serviceType, () => Construct(serviceGetters));
+            return true;
+        }
+    }
+}
